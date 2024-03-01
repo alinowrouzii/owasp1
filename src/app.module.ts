9de8152fa7289a0a78b6fd2c8e8489599c361940
Ali Nowrouzi <alinowrouzii@gmail.com>
2024-03-01 10:27:32 -0800

    chore: add transformers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './modules/database/database.module';
+import { OtpModule } from './modules/otp/otp.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath: 'env/.env',
+      isGlobal: true,
+    }),
+    OtpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
