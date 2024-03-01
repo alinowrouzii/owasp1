22b04db443a871655f4e2825f204c3144e879014
Ali Nowrouzi <alinowrouzii@gmail.com>
2024-03-01 10:55:01 -0800

    chore: add api prefix and versioning

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  // enable versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
